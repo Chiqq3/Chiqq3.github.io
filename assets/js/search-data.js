7aca16e2f1da0daaeacd98d9a7b1a244d09cb674
Chiqq3 <chiweiqitay@gmail.com>
2025-10-20 03:03:33 +0000

    Deploying to gh-pages from @ Chiqq3/Chiqq3.github.io@7f02a7f00b5cf5a257101397f82ba8c292bd7815 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -419,6 +419,11 @@ ninja.data = [{
           description: "",
           section: "Books",handler: () => {
               window.location.href = "/books/the_godfather/";
+            },},{id: "news-a-long-announcement-with-details",
+          title: 'A long announcement with details',
+          description: "",
+          section: "News",handler: () => {
+              window.location.href = "/news/announcement_2/";
             },},{id: "news-my-personal-logo-is-born",
           title: 'My Personal Logo is Born! 🎨',
           description: "",
