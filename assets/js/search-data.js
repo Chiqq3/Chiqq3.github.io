422468818f880c8aa1066701babec2519f2cc4f9
Chiqq3 <chiweiqitay@gmail.com>
2025-10-28 03:47:23 +0000

    Deploying to gh-pages from @ Chiqq3/Chiqq3.github.io@c00980d2d851a9aa9e91194bc96c9dd40e48ca0b 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -436,8 +436,8 @@ ninja.data = [{
           description: "",
           section: "News",handler: () => {
               window.location.href = "/news/2025-10-20-logo/";
-            },},{id: "projects-wisj-ml-summer-school-intellirsu-project",
-          title: 'WISJ ML Summer School - IntelliRSU Project',
+            },},{id: "projects-wisj-ml-summer-school",
+          title: 'WISJ ML Summer School',
           description: "",
           section: "Projects",handler: () => {
               window.location.href = "/projects/WISJ_Leisure/";
