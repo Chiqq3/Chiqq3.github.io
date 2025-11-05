1a631215b4861b8b8b9ce920cd1af06276b779f6
Chiqq3 <chiweiqitay@gmail.com>
2025-11-05 13:11:21 +0000

    Deploying to gh-pages from @ Chiqq3/Chiqq3.github.io@ac5d14c9b1c1daaa81e22a54b8144b6e4b78a855 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -102,6 +102,17 @@ ninja.data = [{
             window.location.href = "/blog/2024/tabs/";
           
         },
+      },{id: "post-a-post-with-typograms",
+        
+          title: "a post with typograms",
+        
+        description: "this is what included typograms code could look like",
+        section: "Posts",
+        handler: () => {
+          
+            window.location.href = "/blog/2024/typograms/";
+          
+        },
       },{id: "post-a-post-with-audios",
         
           title: "a post with audios",
