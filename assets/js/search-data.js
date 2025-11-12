0158ee04cb35914c4802047ce622d6098ac45f72
Chiqq3 <chiweiqitay@gmail.com>
2025-11-12 12:01:53 +0000

    Deploying to gh-pages from @ Chiqq3/Chiqq3.github.io@5ea5236c13e8c014d61ca404cc111fc709900a3e 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -91,17 +91,6 @@ ninja.data = [{
             window.location.href = "/blog/2023/audios/";
           
         },
-      },{id: "post-a-post-with-videos",
-        
-          title: "a post with videos",
-        
-        description: "this is what included videos could look like",
-        section: "Posts",
-        handler: () => {
-          
-            window.location.href = "/blog/2023/videos/";
-          
-        },
       },{id: "books-the-godfather",
           title: 'The Godfather',
           description: "",
